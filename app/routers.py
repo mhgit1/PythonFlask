95a07ba3356d888502847c23a9729b507f92e923
mhgit1 <mkkha7@gmail.com>
2016-01-27 14:43:12 +0200

    First commit

---
+++ app/routers.py
@@ -0,0 +1,32 @@
+from app import app 
+"""Monirivinen kommentti: Ensimmäinen app viittaa app -kansioon 
+    ja toinen __init__.py -tiedostossa määritettyyn app -nimeen"""
+#Yksirivinen kommentti: importilla voi tuoda toisessa tiedostossa määritettyjä moduuleja
+
+#render_template gives you access to Jinja2 template engine
+#request -objektilla saadaan requestin käsittely routeille
+#make_response -objektilla voidaan lisätä 'header' -tietojen käsittely pyynnöille
+from flask import render_template,request,make_response
+
+@app.route('/')
+def index():
+    name = 'Jussi'
+    address = 'Jokukatu 1'
+    response = make_response(render_template('template_index.html',name=name,title=address))
+    response.headers.add('Cache-Control','no-cache')
+    return response
+    #return render_template('template_index.html',title=address,name=name)
+    #return 'Hello World'
+    
+@app.route('/user/<username>')
+def user(username):
+    print(request.headers.get('User-Agent'))
+    print(request.headers.get('Accept-Language'))
+    print(request.headers.get('Accept-Encoding'))
+    return render_template('template_user.html',name=username)
+
+#Tälle routelle voidaan lähettää dataa GET ja POST -metodeilla. GET on default, jos ei ole määritetty.
+@app.route('/user',methods=['GET','POST'])
+def userParams():
+    name = request.args.get('name')   #luetaan /user -kontekstiin tulevasta pyynnöstä attribuutti 'name'
+    return render_template('template_user.html',name=name)
---
+++ config.py
@@ -0,0 +1,3 @@
+SERVER_NAME='localhost:3000'
+
+""" Täällä voidaan määritellä esim. kansiorakenne-, email- ja security -asetukset"""
